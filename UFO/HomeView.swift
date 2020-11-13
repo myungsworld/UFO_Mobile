f5f8d7b407e7e8a14700a478551cb920eb3dc58e
myungsworld <myungsworld@gmail.com>
2020-11-13 12:33:38 +0900

    [ADD] myQRcodeData

--- UFO/HomeView.swift
+++ UFO/HomeView.swift
@@ -7,7 +7,7 @@
 //
 
 import SwiftUI
-//import CodeScanner
+import CodeScanner
 import Combine
 import CoreImage.CIFilterBuiltins
 
@@ -58,10 +58,25 @@ struct HomeView: View {
     @State private var amount : String = "100"
     @State private var org : String = "CustomerOrg"
     
+    var http = HttpAuth()
+    
     @State private var name  = "송동명"
     @State private var email = "myungsworld@gmail.com"
     
-    var http = HttpAuth()
+    let context = CIContext()
+    let filter = CIFilter.qrCodeGenerator()
+    
+    func generateQRcode(from string: String) ->UIImage {
+        let data = Data(string.utf8)
+        filter.setValue(data, forKey: "inputMessage")
+        
+        if let outputImage = filter.outputImage {
+            if let cgimg = context.createCGImage(outputImage, from: outputImage.extent) {
+                return UIImage(cgImage: cgimg)
+            }
+        }
+        return UIImage(systemName: "xmark.circle") ?? UIImage()
+    }
     
     var body: some View {
         ZStack(alignment: .top) {
@@ -77,6 +92,10 @@ struct HomeView: View {
                         .textContentType(.emailAddress)
                         .font(.title)
                         .padding([.horizontal, .bottom])
+                    Image(uiImage: generateQRcode(from: "\(name)\n\(email)"))
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 200, height: 200)
                     Spacer()
                 }
             }
